14972c3d93e6a8855a579e24292235e0fcba67ae
shamantula <78084632+shamantula@users.noreply.github.com>
2025-08-21 13:29:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,60 @@
+# --- provider ---
+provider "aws" {
+  region = "ap-south-1"   # change to your region
+}
+
+# --- get default VPC ---
+data "aws_vpc" "default" {
+  default = true
+}
+
+# --- get default subnet(s) ---
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# --- security group (allow SSH + HTTP) ---
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- EC2 Instance ---
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c2af51e265bd5e0f"   # Ubuntu 22.04 LTS (Mumbai region, change if needed)
+  instance_type = "t2.micro"
+
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+}
