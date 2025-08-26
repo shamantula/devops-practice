a5b37d683b944c74b721b942438985d5ca07b83c
shamantula <78084632+shamantula@users.noreply.github.com>
2025-08-26 15:22:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,12 @@ data "aws_vpc" "default" {
 }
 
 # --- get default subnet(s) ---
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
+#data "aws_subnets" "default" {
+  #filter {
+   # name   = "vpc-id"
+   # values = [data.aws_vpc.default.id]
+  #}
+#}
 
 # --- security group (allow SSH + HTTP) ---
 resource "aws_security_group" "ec2_sg" {
@@ -54,12 +54,12 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0f918f7e67a3323f0"   # Ubuntu 22.04 LTS (Mumbai region, change if needed)
   instance_type = "t3.micro"
 
-  subnet_id              = data.aws_subnets.default.ids[0]
+  subnet_id              = subnet-0fc1a306d66f5c7cd
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-  count = 3   # <-- This creates 3 EC2 instances
+  count = 1   # <-- This creates 3 EC2 instances
 key_name = aws_key_pair.my_key.key_name # use the key pair created above
   tags = {
-    Name = "web-${count.index + 1}" # web-1, web-2, web-3
+    Name = "web-1"
   }
 }
